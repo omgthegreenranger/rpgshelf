4ee4cbdccedbb2b9eff93bf0b2ac0e103ae79ac0
omgthegreenranger <das.shaggy@gmail.com>
2023-04-21 19:43:07 -0400

    File API is working

--- backend/base.py
+++ backend/base.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, jsonify
 import pikepdf
 import fs
+from fs.osfs import OSFS
+import os
 
 api = Flask(__name__)
 
-@app.route('/readfile', methods=["POST"])
-def add_guide():
-    title = request.json['title']
+@api.route('/readfile', methods=["POST"])
+def read_file():
+    pdfPath = request.json['path']
+    pdf = pikepdf.open(pdfPath)
+    meta = pdf.open_metadata()
+    print(meta['dc:title'])
+    response = read.jsonify({'title': meta['dc:title']})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
