712e065c0dff4ebe8e081bebee6891f3c1cbfe92
omgthegreenranger <das.shaggy@gmail.com>
2024-01-31 11:27:21 -0500

    SQLalchemy table creation scripts

--- backend/db/sqlalchemy_scripts.py
+++ backend/db/sqlalchemy_scripts.py
@@ -1,14 +1,48 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+url = URL.create(
+    drivername="postgresql",
+    username="shaggy",
+    host="localhost",
+    database="rpgshelf"
+)
+engine = create_engine(url, echo=True)
+connection = engine.connect()
 
-with engine.connect() as conn:
-    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
-    conn.execute(
-        text("INSERT INTO some_table (x, y) VALUES  (:x, :y)"),
-        [{"x": 1, "y": 1}, {"x":2, "y":4}],
-    )
-    conn.commit()
-    result = conn.execute(text("SELECT x, y FROM some_table"))
-    for row in result:
-        print(f"x: {row.x}  y: {row.y}")
+
+from sqlalchemy import Column, Integer, String, DateTime, Text
+from sqlalchemy.orm import declarative_base, relationship, backref
+from datetime import datetime
+
+Base = declarative_base()
+
+class System(Base):
+    __tablename__ = 'systems'
+
+    gid = Column(Integer(), primary_key=True)
+    system_name = Column(String(100), nullable=False, unique=True)
+    rid = Column(Integer(), nullable=True, unique=True)
+    system = Column(String(100))
+    description = Column(Text)
+    edition = Column(String(50), nullable = True)
+    library = relationship('Book', backref='system')
+
+
+class Book(Base):
+    __tablename__ = 'library'
+    
+    bid = Column(Integer(), primary_key=True)
+    brid = Column(Integer())
+    book_title = Column(String(150))
+    asset_type = Column(String(100))
+    description = Column(Text)
+    path = Column(String(200))
+    image = Column(String(200))
+    publisher = Column(String(200))
+    designers = Column(String(200))
+    artists = Column(String(200))
+    producers = Column(String(200))
+    year = Column(DateTime())
+
+Base.metadata.create_all(engine)
